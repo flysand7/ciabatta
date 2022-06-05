7b6bea0ff15c2fc2d33dfc265152adadcd16c55e
bumbread <thebumboni@gmail.com>
2022-06-06 09:02:54 +1100

    strtod, strtof, strtold

--- test/test.c
+++ test/test.c
@@ -1,44 +1,27 @@
-#include <assert.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdbool.h>
+#include <stdio.h>
 #include <errno.h>
+#include <stdlib.h>
+ 
+int main(void)
+{
+    double v0 = strtod("0X1.BC70A3D70A3D7P+6", NULL);
 
-int main(int argc, char** argv) {
-    const char *p = "10 200000000000000000000000000000 30 -40 junk";
-    // printf("Parsing '%s':\n", p);
- 
-    for (;;)
+    // parsing with error handling
+    const char *p = "111.11 -2.22 Nan nan(2) inF 0X1.BC70A3D70A3D7P+6  1.18973e+4932zzz";
+    
+    char *end;
+    for (double f = strtod(p, &end); p != end; f = strtod(p, &end))
     {
-        // errno can be set to any non-zero value by a library function call
-        // regardless of whether there was an error, so it needs to be cleared
-        // in order to check the error set by strtol
-        errno = 0;
-        char *end;
-        const long i = strtol(p, &end, 10);
-        if (p == end)
-            break;
- 
-        const bool range_error = errno == ERANGE;
-        // printf("Extracted '%.*s', strtol returned %ld.", (int)(end-p), p, i);
+        // printf("'%.*s' -> ", (int)(end-p), p);
         p = end;
- 
-        // if (range_error)
-        //     printf(" Range error occurred.");
- 
-        // putchar('\n');
+        if (errno == ERANGE){
+            // printf("range error, got ");
+            errno = 0;
+        }
+        // printf("%f\n", f);
     }
  
-    // printf("Unextracted leftover: '%s'\n\n", p);
- 
     // parsing without error handling
-    long v1 = strtol("1010", NULL, 2);
-    long v2 = strtol("12",   NULL, 8);
-    long v3 = strtol("A",    NULL, 16);
-    long v4 = strtol("junk", NULL, 36);
-    long v5 = strtol("012",  NULL, 0);
-    long v6 = strtol("0xA",  NULL, 0);
-    long v7 = strtol("junk", NULL, 0);
-    return 0;
+    double v1 = strtod("  -0.0000000123junk", NULL);
+    double v2 = strtod("junk", NULL);
 }
