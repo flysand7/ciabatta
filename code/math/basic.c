aa4c3c2d3271abc9a02ac3d4d5cbc6f0b0c375ac
bumbread <thebumboni@gmail.com>
2022-06-09 09:05:50 +1100

    LESS FILES

--- code/math/basic.c
+++ code/math/basic.c
@@ -1,4 +1,20 @@
 
+#include <math.h>
+
+double fabs(double x) {
+    if(x >= 0) {
+        return x;
+    }
+    return -x;
+}
+
+float fabsf(float x) {
+    if(x >= 0) {
+        return x;
+    }
+    return -x;
+}
+
 double fmod(double x, double y) {
     int n = 0;
     while(y < x*n) {
