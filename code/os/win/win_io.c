5890659112e2bcc06754b042bc9b1855cde2fe03
bumbread <thebumboni@gmail.com>
2022-06-09 20:09:01 +1100

    tabs ==> spaces

--- code/os/win/win_io.c
+++ code/os/win/win_io.c
@@ -97,29 +97,29 @@ int system(const char* string) {
     MultiByteToWideChar(65001 /* UTF8 */, 0, string, -1, cmd_line + sizeof("cmd.exe ") - 1, wchars_required);
 
     STARTUPINFOW si = {
-		.cb = sizeof(STARTUPINFOW),
-		.dwFlags = STARTF_USESTDHANDLES,
-		.hStdInput = GetStdHandle(STD_INPUT_HANDLE),
-		.hStdError = GetStdHandle(STD_ERROR_HANDLE),
-		.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE)
-	};
-	PROCESS_INFORMATION pi = {};
+        .cb = sizeof(STARTUPINFOW),
+        .dwFlags = STARTF_USESTDHANDLES,
+        .hStdInput = GetStdHandle(STD_INPUT_HANDLE),
+        .hStdError = GetStdHandle(STD_ERROR_HANDLE),
+        .hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE)
+    };
+    PROCESS_INFORMATION pi = {};
 
     if (!CreateProcessW(NULL, cmd_line, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
         goto error;
     }
 
-	// Wait until child process exits.
-	WaitForSingleObject(pi.hProcess, INFINITE);
+    // Wait until child process exits.
+    WaitForSingleObject(pi.hProcess, INFINITE);
 
     DWORD exit_code;
     if (!GetExitCodeProcess(pi.hProcess, &exit_code)) {
         goto error;
     }
 
-	// Close process and thread handles.
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
+    // Close process and thread handles.
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
     free(cmd_line);
     return exit_code;
 
