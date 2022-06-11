effd0acb63a7802ab9113d47c71eaecc73213c4e
bumbread <thebumboni@gmail.com>
2022-06-11 17:17:24 +1100

    math.h rounding functions

--- test/test_math.c
+++ test/test_math.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <float.h>
 #include <fenv.h>
+#include <inttypes.h>
 
 const char *show_classification(double x) {
     switch(fpclassify(x)) {
@@ -55,22 +56,52 @@ int main() {
     printf("rint(1.1) = %f\n", rint(1.1));
     if(fetestexcept(FE_INEXACT)) printf("    FE_INEXACT was raised\n");
 
-
+    printf("\n\n=== nextafter === \n");
     float from1 = 0, to1 = nextafterf(from1, 1);
     printf("The next representable float after %f is %f\n", from1, to1);
     float from2 = 1, to2 = nextafterf(from2, 2);
     printf("The next representable float after %f is %f\n", from2, to2);
-    {
-        #pragma STDC FENV_ACCESS ON
-        feclearexcept(FE_ALL_EXCEPT);
-        double from4 = DBL_MAX, to4 = nextafter(from4, INFINITY);
-        printf("The next representable double after %f is %f\n",
-               from4, to4);
-        if(fetestexcept(FE_OVERFLOW)) printf("   raised FE_OVERFLOW\n");
-        if(fetestexcept(FE_INEXACT))  printf("   raised FE_INEXACT\n");
-    }
     float from5 = 0.0, to5 = nextafter(from5, -0.0);
     printf("nextafter(+0.0, -0.0) gives %f (%f)\n", to5, to5);
+
+    printf("\n\n=== ceil === \n");
+    printf("ceil(+2.4) = %f\n", ceil(2.4));
+    printf("ceil(-2.4) = %f\n", ceil(-2.4));
+    printf("ceil(-0.0) = %f\n", ceil(-0.0));
+    printf("ceil(-Inf) = %f\n", ceil(-INFINITY));
+
+    printf("\n\n=== floor === \n");
+    printf("floor(+2.7) = %f\n", floor(2.7));
+    printf("floor(-2.7) = %f\n", floor(-2.7));
+    printf("floor(-0.0) = %f\n", floor(-0.0));
+    printf("floor(-Inf) = %f\n", floor(-INFINITY));
+
+    printf("\n\n=== trunk === \n");
+    printf("trunc(+2.7) = %f\n", trunc(2.7));
+    printf("trunc(-2.7) = %f\n", trunc(-2.7));
+    printf("trunc(-0.0) = %f\n", trunc(-0.0));
+    printf("trunc(-Inf) = %f\n", trunc(-INFINITY));
+
+    printf("\n\n=== round === \n");
+    printf("round(+2.3)  = %f  ",  round(2.3));
+    printf("round(+2.5)  = %f  ",  round(2.5));
+    printf("round(+2.7)  = %f\n",  round(2.7));
+    printf("round(-2.3)  = %f  ",  round(-2.3));
+    printf("round(-2.5)  = %f  ",  round(-2.5));
+    printf("round(-2.7)  = %f\n",  round(-2.7));
+    printf("round(-0.0)  = %f\n",  round(-0.0));
+    printf("round(-Inf)  = %f\n",  round(-INFINITY));
+    printf("lround(+2.3) = %ld  ", lround(2.3));
+    printf("lround(+2.5) = %ld  ", lround(2.5));
+    printf("lround(+2.7) = %ld\n", lround(2.7));
+    printf("lround(-2.3) = %ld  ", lround(-2.3));
+    printf("lround(-2.5) = %ld  ", lround(-2.5));
+    printf("lround(-2.7) = %ld\n", lround(-2.7));
+    printf("lround(-0.0) = %ld\n", lround(-0.0));
+    printf("lround(-Inf) = %ld\n", lround(-INFINITY));
+    feclearexcept(FE_ALL_EXCEPT);
+    printf("lround(LONG_MAX+1.5) = %ld\n", lround(LONG_MAX+1.5));
+    if(fetestexcept(FE_INVALID)) printf("    FE_INVALID was raised\n");
 
     return 0;
 }
