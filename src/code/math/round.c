a16e2d5a6e6b50592ea45dd6b5c01fe3ccfad87c
bumbread <thebumboni@gmail.com>
2022-06-16 23:39:50 +1100

    bababbababababababa

--- src/code/math/round.c
+++ src/code/math/round.c
@@ -5,7 +5,15 @@
 #include <float.h>
 #include <limits.h>
 
-#include "_util.h"
+#define asuint64(x) ((union {double f; uint64_t i;}){x}).i
+#define asdouble(x) ((union {double f; uint64_t i;}){x}).f
+
+#include <_compiler.h>
+#if defined(_compiler_clang) || defined(_compiler_gcc)
+    #define just_do_it(t) __attribute__((unused)) volatile t
+#else
+    #define just_do_it(t) volatile t
+#endif
 
 double nearbyint(double x) {
     #pragma STDC FENV_ACCESS ON
