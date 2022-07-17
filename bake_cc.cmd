3ab1c1ad299b9bf21080a4be158c7d4c96f65529
bumbread <thebumboni@gmail.com>
2022-07-18 08:32:02 +1100

    say no to make, say no to shell scripts, python is the way to go

--- bake_cc.cmd
+++
@@ -1,8 +0,0 @@
-for /R src\%PLATFORM% %%F in (*.c) do (
-    echo %%F
-    start /B clang -I unicope\inc -Isrc/win -c -o bin\%PLATFORM%\%%~nF.obj %%F %CIABATTA_OPTIONS%
-)
-for /R src\code %%F in (*.c) do (
-    echo %%F
-    start /B clang -I unicope\inc -c -o bin\%%~nF.obj %%F %CIABATTA_OPTIONS%
-)
