5cad924d6884689b0e415eaa3427a77837ce858e
NeGate <yasserarguelles2506@gmail.com>
2023-01-27 22:27:44 -0500

    Update bake.cmd

--- bake.cmd
+++ bake.cmd
@@ -2,12 +2,12 @@
 :: Compile UTF-8 resource into .obj file
 :: this .obj file has to be linked to the executable using it, NOT archived
 :: together with ciabatta.lib.
-rem windres -o utf8.obj utf8.rc
+windres -o utf8.obj utf8.rc
 
 :: Compile chkstk
 nasm src\os_win\chkstk.asm -ochkstk.o -fwin64
 
 :: Compile the rest of the party
-rem clang -Wall src\ciabatta.c -o ciabatta.obj -c -DCIABATTA_WIN -I inc -I src\_win -nodefaultlibs -g -mfma
-cuik src\ciabatta.c -o ciabatta.obj -c -DCIABATTA_WIN -I inc -I src\_win -nostdlib
-lib /nologo /out:ciabatta.lib chkstk.o ciabatta.obj
+clang -Wall src\ciabatta.c -o ciabatta.obj -c -DCIABATTA_WIN -I inc -I src\_win -nodefaultlibs -g -mfma
+rem cuik src\ciabatta.c -o ciabatta.obj -c -DCIABATTA_WIN -I inc -I src\_win -nostdlib
+lib /nologo /out:ciabatta.lib chkstk.o utf8.obj ciabatta.obj
