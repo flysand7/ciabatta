dc3bab6eb59d801fc5e28e4462c88bf4dbf7f752
bumbread <thebumboni@gmail.com>
2023-02-16 06:02:09 +1100

    Implement some time.h functions

--- inc/time.h
+++ inc/time.h
@@ -1,14 +1,27 @@
 #pragma once
 
-#include <stdint.h>
+// 7.29.1 p.2: Macros
+
+#define __STDC_VERSION_TIME_H__ 202311L
 
 #if !defined(NULL)
     #define NULL ((void *)0)
 #endif
 
-#define TIME_UTC 1
-
 #define CLOCKS_PER_SEC  ((clock_t)1000000)
+
+#define TIME_UTC 1
+#define TIME_MONOTONIC 2
+#define TIME_ACTIVE 3
+#define TIME_THREAD_ACTIVE 4
+
+// 7.29.1 p.4: Types
+
+#if defined(_WIN32)
+    typedef unsigned long long size_t;
+#else
+    typedef unsigned long size_t;
+#endif
 
 typedef unsigned long long clock_t;
 typedef unsigned long long time_t;
@@ -36,28 +49,30 @@ typedef unsigned long long time_t;
     };
 #endif
 
-clock_t    clock       (void);
-time_t     time        (time_t *timer);
-time_t     mktime      (struct tm *timeptr);
-double     difftime    (time_t time1, time_t time0);
-int        timespec_get(struct timespec *ts, int base);
-char      *asctime     (const struct tm *timeptr);
-char      *ctime       (const time_t *timer);
-struct tm *gmtime      (const time_t *timer);
-struct tm *localtime   (const time_t *timer);
+// Basic timer functions
+clock_t clock(void);
+time_t time(time_t *timer);
+double difftime(time_t time1, time_t time0);
 
-size_t strftime(
-    char *restrict s,
-    size_t maxsize,
-    const char *restrict format,
-    const struct tm *restrict timeptr
-);
+// Human-readable time functions
+time_t mktime(struct tm *timeptr);
+struct tm *gmtime(const time_t *timer);
+struct tm *localtime(const time_t *timer);
+
+// Timespec functions
+int timespec_get(struct timespec *ts, int base);
+int timespec_getres(struct timespec *ts, int base);
+
+// Time formatting functions
+char *asctime(const struct tm *timeptr);
+char *ctime(const time_t *timer);
+size_t strftime(char *restrict str, size_t sz, const char *restrict fmt, const struct tm *restrict time);
 
 #ifdef __STDC_WANT_LIB_EXT1__
 
 errno_t asctime_s(char *s, rsize_t maxsize, const struct tm *timeptr);
-errno_t ctime_s  (char *s, rsize_t maxsize, const time_t *timer);
-struct tm *gmtime_s   (const time_t * restrict timer, struct tm * restrict result);
+errno_t ctime_s(char *s, rsize_t maxsize, const time_t *timer);
+struct tm *gmtime_s(const time_t * restrict timer, struct tm * restrict result);
 struct tm *localtime_s(const time_t * restrict timer, struct tm * restrict result);
 
 #endif
--- test/test_time.c
+++ test/test_time.c
@@ -1,10 +1,26 @@
-
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
+static const char *const wday[] = {
+    "Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday", "-unknown-"
+};
 
 int main() {
-    time_t t1 = time(NULL);
-    time_t t2 = time(NULL);
-    double s = difftime(t2, t1);
+    // July 4, 2001: Wednesday
+    struct tm time_str;
+    time_str.tm_year = 2001 - 1900;
+    time_str.tm_mon = 7 - 1;
+    time_str.tm_mday = 4;
+    time_str.tm_hour = 0;
+    time_str.tm_min = 0;
+    time_str.tm_sec = 1;
+    time_str.tm_isdst = -1;
+    time_t timestamp;
+    if((timestamp = mktime(&time_str)) == (time_t)(-1)) {
+        time_str.tm_wday = 7;
+    }
+    char const *day_of_week = wday[time_str.tm_wday];
+    struct tm *gmt = gmtime(&timestamp);
+    struct tm *local = localtime(&timestamp);
     return 0;
 }
