9c643bd0a87a1882279b0c056b91397d89c8d275
flysand7 <yyakut.ac@gmail.com>
2023-06-18 18:28:07 +1100

    rename some files

--- build.cmd
+++ build.cmd
@@ -8,6 +8,6 @@ windres -o utf8.obj utf8.rc
 nasm src\os_win\chkstk.asm -ochkstk.o -fwin64
 
 :: Compile the rest of the party
-clang -Wall src\ciabatta.c -o ciabatta.obj -c -DCIABATTA_WIN -I src -I inc -nodefaultlibs -g -mfma
+clang -Wall src\ciabatta.c -o ciabatta.obj -c -DCIABATTA_WIN -I src -I include -nodefaultlibs -g -mfma
 rem cuik src\ciabatta.c -o ciabatta.obj -c -DCIABATTA_WIN -I src -I inc -nostdlib
 lib /nologo /out:ciabatta.lib chkstk.o utf8.obj ciabatta.obj
